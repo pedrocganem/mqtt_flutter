fab7f053d836a3782b8ed635b4d8733d1b609f5d
Pedro Ganem <pedrocastilho.ganemg@gmail.com>
2020-10-26 19:19:55 -0300

    broker class and setup function

---
+++ lib/mqtt.dart
@@ -0,0 +1,79 @@
+import 'dart:async';
+import 'package:mqtt_client/mqtt_client.dart';
+import 'package:mqtt_client/mqtt_server_client.dart';
+
+class Broker {
+  static final Broker _singleton = Broker._internal();
+
+  factory Broker() {
+    return _singleton;
+  }
+
+  Broker._internal();
+
+  //Define class variables:
+
+  String broker = '';
+  int port = 1883;
+  String username;
+  String password;
+  String clientIdentifier;
+  MqttServerClient client;
+  StreamSubscription subscription;
+
+  Future<MqttServerClient> brokerSetup(Function function) async {
+    client = MqttServerClient.withPort(broker, clientIdentifier, port);
+    client.logging(on: true);
+    client.onConnected = onConnected;
+    client.onDisconnected = onDisconnected;
+    client.onSubscribed = onSubscribed;
+    client.onSubscribeFail = onSubscribeFail;
+    client.pongCallback = pong;
+    client.secure = false;
+
+    final connMessage = MqttConnectMessage()
+        .authenticateAs(username, password)
+        .keepAliveFor(60)
+        .startClean()
+        .withWillQos(MqttQos.atLeastOnce)
+        .withClientIdentifier(clientIdentifier);
+
+    client.connectionMessage = connMessage;
+
+    try {
+      await client.connect();
+      // client.subscribe(topic, qosLevel)
+    } catch (e) {
+      print('Exception: $e');
+      client.disconnect();
+    }
+
+    subscription = client.updates.listen(function);
+
+    return client;
+  }
+}
+
+void onConnected() {
+  print('connected');
+}
+
+void onDisconnected() {
+  print('disconnected');
+}
+
+void onSubscribed(String topic) {
+  print('subscribed to $topic');
+}
+
+void onSubscribeFail(String topic) {
+  print('failed to subscribe to $topic');
+}
+
+void on() {
+  print('disconnected');
+}
+
+void pong() {
+  print('ping response arrived');
+}

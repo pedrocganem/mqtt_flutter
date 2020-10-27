67833340a9bcb7d96b6a445470fe9ac0c2f913bd
Pedro Ganem <pedrocastilho.ganemg@gmail.com>
2020-10-27 17:40:23 -0300

    added connect button and listener setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:mqtt_client/mqtt_client.dart';
+import 'package:mqtt_flutter/mqtt.dart';
 
 void main() {
   runApp(HomeScreen());
@@ -11,6 +13,25 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   String messageFromBroker;
+  bool isConnected;
+  String connectButtonTitle;
+
+  void onMessage(List<MqttReceivedMessage<MqttMessage>> event) {
+    final MqttPublishMessage recMess = event[0].payload as MqttPublishMessage;
+    final String message =
+        MqttPublishPayload.bytesToStringAsString(recMess.payload.message);
+    print('[MQTT client] MQTT message: topic is <${event[0].topic}>');
+    print("[MQTT client] message with topic: ${event[0].topic}");
+    print("[MQTT client] message with message: $message");
+
+    if (message != null) {
+      isConnected = true;
+    }
+    setState(() {
+      messageFromBroker = message;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,11 +39,21 @@ class _HomeScreenState extends State<HomeScreen> {
       appBar: AppBar(
         backgroundColor: Colors.white,
         elevation: 0,
-        title: Text('Using MQTT on Flutter!'),
+        title: Text('Using MQTT with flutter!'),
       ),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           Text(messageFromBroker),
+          FlatButton(
+              onPressed: () {
+                Broker().brokerSetup(onMessage);
+                setState(() {
+                  connectButtonTitle = 'connecting';
+                });
+              },
+              child: Text(connectButtonTitle))
         ],
       ),
     );
